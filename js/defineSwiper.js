5efdb7a0ce8bc5fa21406699d719649a7febf84b
lf <1063477160@qq.com>
2020-03-29 16:25:48 +0800

    分页切换

--- js/defineSwiper.js
+++ js/defineSwiper.js
@@ -1,21 +1,24 @@
-let masks=$(".mask_box .mask");
+let masks = $(".mask_box .mask");
 var swiper = new Swiper('.swiper-container', {
-    autoplay: {
-        disableOnInteraction: false, //手动滑动之后不打断播放
-        delay: 1000
-    },
+    // autoplay: {
+    //     disableOnInteraction: false, //手动滑动之后不打断播放
+    //     delay: 1000
+    // },
     loop: true,
+    on: {
+        transitionStart: function () {
+            this.active = $('.swiper-slide-active').attr('data-swiper-slide-index')
+            var sum = this.realIndex;
+            masks.eq(sum).css({
+                opacity: 1
+            }).siblings().css({
+                opacity: 0
+            })
+            
+        },
+    },
     pagination: {
         el: '.swiper-pagination',
         clickable: true
     },
-    on: {
-        slideChangeTransitionEnd() { 
-            this.active = $('.swiper-slide-active').attr('data-swiper-slide-index')
-            var sum = this.realIndex;
-            console.log(sum)
-            masks.eq(sum).css({opacity:1}).siblings().css({opacity:0})
-        }
-    }
-
 });
